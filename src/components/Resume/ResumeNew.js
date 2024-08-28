b591dc91c7594f8b6a1123a2cb4b7170b225c799
Vivek Pratap <vivekpratapvp11@gmail.com>
2024-08-29 00:56:04 +0530

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,16 +1,74 @@
+// import React, { useState, useEffect } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import Particle from "../Particle";
+// import pdf from "../../Assets/../Assets/VIVEK'S_CV.pdf";
+// import cv from "../../Assets/../Assets/cv.png";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// const resumeLink =
+//   "https://github.com/VIVEKPRATAP11/Portfolio/blob/master/src/Assets/VIVEK'S_CV.pdf";
+
+// function ResumeNew() {
+//   const [width, setWidth] = useState(1200);
+
+//   useEffect(() => {
+//     setWidth(window.innerWidth);
+//   }, []);
+
+//   return (
+//     <div>
+//       <Container fluid className="resume-section">
+//         <Particle />
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
+
+//         <Row className="resume">
+//           <img src={cv} alt="cv" className="cv" />
+//           {/* <Document file={resumeLink} className="d-flex justify-content-center">
+//             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+//           </Document> */}
+//         </Row>
+
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default ResumeNew;
+
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/VIVEK'S_CV.pdf";
-import cv from "../../Assets/../Assets/cv.png";
+import pdf from "../../Assets/Vivek_resume_WebDev.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-const resumeLink =
-  "https://github.com/VIVEKPRATAP11/Portfolio/blob/master/src/Assets/VIVEK'S_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -36,10 +94,9 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <img src={cv} alt="cv" className="cv" />
-          {/* <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document> */}
+          </Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
